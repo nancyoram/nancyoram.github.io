06df5f3c4c19d53abe1dd70d436c18a50d35605c
nancyoram <ora15002@byui.edu>
2019-02-23 13:39:23 -0800

    commit

--- assignments/lesson-7/scripts/currentdate.js
+++ assignments/lesson-7/scripts/currentdate.js
@@ -1,69 +1,12 @@
-var today = new Date();
-var day = today.getDay();
-var month = today.getMonth() + 1;
-var year = today.getFullYear();
-var date = today.getDate();
+var d = new Date();
 
-switch (day) {
-case 1:
-    day = "Monday";
-    break;
-case 2:
-    day = "Tuesday";
-    break;
-case 3:
-    day = "Wednesday";
-    break;
-case 4:
-    day = "Thursday";
-    break;
-case 5:
-    day = "Friday";
-    break;
-case 6:
-    day = "Saturday";
-    break;
-default:
-    day = "Sunday";
-}
+var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-switch (month) {
-case 1:
-    month = "January";
-    break;
-case 2:
-    month = "Febuary";
-    break;
-case 3:
-    month = "March";
-    break;
-case 4:
-    month = "April";
-    break;
-case 5:
-    month = "May";
-    break;
-case 6:
-    month = "June";
-    break;
-case 7:
-    month = "July";
-    break;
-case 8:
-    month = "August";
-    break;
-case 9:
-    month = "September";
-    break;
-case 10:
-    month = "October";
-    break;
-case 11:
-    month = "November";
-    break;
-case 12:
-    month = "December";
-    break;
-}
-var displayDate = day + ", " + date + " " + month + " " + year;
-document.getElementById("currentDate").innerHTML = displayDate;
+var weekday = weekdays[d.getDay()];
+var day = d.getDate();
+var month = months[d.getMonth()];
+var year = d.getFullYear();
+
+var n = weekday + ", " + day + " " + month + " " + year;
+document.getElementById("currentdate").innerHTML = n;
--- assignments/lesson-7/scripts/hamburger-6.js
+++
@@ -1,4 +0,0 @@
-function toggleHam() {
-    "use strict";
-	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
-}
