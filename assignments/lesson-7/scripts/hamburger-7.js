d4cfbd2fa88976baa983d6d8acf84f81eb959d36
nancyoram <ora15002@byui.edu>
2019-02-28 20:42:50 -0800

    commit

--- assignments/lesson-7/scripts/hamburger-7.js
+++ assignments/lesson-7/scripts/hamburger-7.js
@@ -1,3 +1,8 @@
-function toggleMenu() {
-  document.getElementById("primaryNav").classList.toggle("hide");
-}                     
+function toggleNavMenu() {
+  var x = document.getElementById("hamburger");
+  if (x.className === "topnav") {
+    x.className = "responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
